f59c237280cfe46cdabd7e023e31ac208f95d23c
284924 <284924@eriesd.org>
2023-01-12 14:27:16 -0500

    B

--- random_walk.py
+++ random_walk.py
@@ -5,7 +5,7 @@ class RandomWalk:
         self.x_values = [0]
         self.y_values = [0]
 
-    def get_step(self):
+    def get_step(self):  
         direction = choice([-1, 1])
         distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
         return[direction*distance]
@@ -29,6 +29,8 @@ class RandomWalk:
 
             x = self.x_values[-1] + x_step
             y = self.y_values[-1] + y_step
+   
+
    
             self.x_values.append(x)
             self.y_values.append(y)
